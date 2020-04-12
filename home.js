21bbdbfad9c0548706597520381f5886ecdd9e2c
Juan David Hernández <jhernandez@sava.cc>
2020-04-12 18:06:18 -0500

    Corrigiendo detalles

--- home.js
+++ home.js
@@ -192,7 +192,7 @@ function empezarJuego() {
 function infoJuego() {
   swal({
     title: "¿Como se juega?",
-    text: "El juego de forma aleatoria va iluminando los cuadrantes de colores. Después de esperar, el usuario debe ir introduciendo la secuencia mostrada en el orden correcto, ayudándose de su memoria visual. Si lo consigue, éste responderá con una secuencia más larga, y así sucesivamente. Si falla, el usuario debe volver a empezar. Los distintos niveles de dificultad van aumentando la velocidad de la secuencia a repetir.",
+    text: "El juego de forma aleatoria va iluminando los cuadrantes de colores, y a la vez que se ilumina cada cuadrante emite un sonido propio. Después de esperar, el usuario debe ir introduciendo la secuencia mostrada en el orden correcto, ayudándose de su memoria visual y sonora. Si lo consigue, éste responderá con una secuencia más larga, y así sucesivamente. Si falla, el usuario debe volver a empezar. Los distintos niveles de dificultad van aumentando la velocidad de la secuencia a repetir.",
     icon: "info",
     button: "Ok",
   });
