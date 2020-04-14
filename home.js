507912219fcbcc23dd6953b6d527e7f1b7b55fd9
Juan David Hernández <jhernandez@sava.cc>
2020-04-13 23:56:42 -0500

    Realizando condicion para funcionar solo con chrome

--- home.js
+++ home.js
@@ -1,106 +1,166 @@
-// Colores
-const celeste = document.getElementById('celeste')
-const violeta = document.getElementById('violeta')
-const naranja = document.getElementById('naranja')
-const verde = document.getElementById('verde')
-// Buton
-const btnEmpezar = document.getElementById('btnEmpezar')
-// Sounds
-var audioDo = document.getElementById("audioDo");
-var audioRe = document.getElementById("audioRe");
-var audioMi = document.getElementById("audioMi");
-var audioFa = document.getElementById("audioFa");
+var es_chrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
+if(es_chrome){
+    // body
 
-var nivelActual = document.getElementById('nivelActual')
-var nivelRecord = document.getElementById('nivelRecord')
-var nivelR = 0;
+    // Paleta
+    const circulo = document.getElementByClassName('gameboard')
+    // Estrellas
+    const estrella1 = document.getElementByClassName('estrella1')
+    const estrella2 = document.getElementByClassName('estrella2')
+    const estrella3 = document.getElementByClassName('estrella3')
+    const estrella4 = document.getElementByClassName('estrella4')
+    const estrella5 = document.getElementByClassName('estrella5')
+    const estrella6 = document.getElementByClassName('estrella6')
+    const estrella7 = document.getElementByClassName('estrella7')
+    const meteoro = document.getElementByClassName('meteoro')
+    // Colores
+    const celeste = document.getElementById('celeste')
+    const violeta = document.getElementById('violeta')
+    const naranja = document.getElementById('naranja')
+    const verde = document.getElementById('verde')
+    // Buton
+    const btnEmpezar = document.getElementById('btnEmpezar')
+    // Sounds
+    var audioDo = document.getElementById("audioDo");
+    var audioRe = document.getElementById("audioRe");
+    var audioMi = document.getElementById("audioMi");
+    var audioFa = document.getElementById("audioFa");
 
-const ULTIMO_NIVEL = 30
+    var nivelActual = document.getElementById('nivelActual')
+    var nivelRecord = document.getElementById('nivelRecord')
+    var nivelR = 0;
 
-swal({
-  title: "RECOMENDACIONES",
-  text: "Para mejor jugabilidad se recomienda el uso del navegador Google Chrome.",
-  icon: "chrome.png",
-  button: "Ok",
-});
+    const ULTIMO_NIVEL = 30
 
-window.onload = function() {
-  window.scrollTo(0, document.body.scrollHeight);
-};
+    swal({
+      title: "RECOMENDACIONES",
+      text: "Para mejor jugabilidad se recomienda el uso del navegador Google Chrome.",
+      icon: "chrome.png",
+      button: "Ok",
+    });
 
-class Juego {
-  constructor() {
-    this.inicializar = this.inicializar.bind(this);
-    this.inicializar();
-    this.generarSecuencia();
-    setTimeout( this.siguienteNivel, 500);
-  }
+    window.onload = function() {
+      window.scrollTo(0, document.body.scrollHeight);
+    };
 
-  inicializar() {
-    this.siguienteNivel = this.siguienteNivel.bind(this)
-    this.elegirColor = this.elegirColor.bind(this);
-    this.toggleBtnEmpezar();
-    this.nivel = 1;
-    nivelActual.innerHTML = this.nivel;
-    this.colores = {
-      celeste,
-      violeta,
-      naranja,
-      verde
-    }
-  }
+    class Juego {
+      constructor() {
+        this.inicializar = this.inicializar.bind(this);
+        this.inicializar();
+        this.generarSecuencia();
+        setTimeout( this.siguienteNivel, 500);
+      }
 
-  toggleBtnEmpezar() {
-    if (btnEmpezar.classList.contains('hide')) {
-      btnEmpezar.classList.remove('hide');
-    } else {
-      btnEmpezar.classList.add('hide');
-    }
-  }
+      inicializar() {
+        this.siguienteNivel = this.siguienteNivel.bind(this)
+        this.elegirColor = this.elegirColor.bind(this);
+        this.toggleBtnEmpezar();
+        this.nivel = 1;
+        nivelActual.innerHTML = this.nivel;
+        this.colores = {
+          celeste,
+          violeta,
+          naranja,
+          verde
+        }
+      }
 
-  generarSecuencia() {
-    this.secuencia = new Array(ULTIMO_NIVEL).fill(0).map(n => Math.floor(Math.random() * 4))
-  }
+      toggleBtnEmpezar() {
+        if (btnEmpezar.classList.contains('hide')) {
+          btnEmpezar.classList.remove('hide');
+        } else {
+          btnEmpezar.classList.add('hide');
+        }
+      }
 
-  siguienteNivel() {
-    this.subnivel = 0
-    this.iluminarSecuencia();
-    this.agregarEventoClick();
-  }
+      generarSecuencia() {
+        this.secuencia = new Array(ULTIMO_NIVEL).fill(0).map(n => Math.floor(Math.random() * 4))
+      }
 
-  transformarNumeroAColor(numero) {
-    switch (numero) {
-      case 0:
-        return 'celeste'
-      case 1:
-        return 'violeta'
-      case 2:
-        return 'naranja'
-      case 3:
-        return 'verde'
+      siguienteNivel() {
+        this.subnivel = 0
+        this.iluminarSecuencia();
+        this.agregarEventoClick();
+      }
 
-    }
-  }
+      transformarNumeroAColor(numero) {
+        switch (numero) {
+          case 0:
+            return 'celeste'
+          case 1:
+            return 'violeta'
+          case 2:
+            return 'naranja'
+          case 3:
+            return 'verde'
 
-  transformaColorANumero(color) {
-    switch (color) {
-      case 'celeste':
-        return 0
-      case 'violeta':
-        return 1
-      case 'naranja':
-        return 2
-      case 'verde':
-        return 3
-    }
-  }
+        }
+      }
 
-  iluminarSecuencia() {
-    for (let i = 0; i < this.nivel; i++) {
-      let color = this.transformarNumeroAColor(this.secuencia[i]);
-      setTimeout(() => {
-        this.iluminarColor(color)
-        switch (this.secuencia[i]) {
+      transformaColorANumero(color) {
+        switch (color) {
+          case 'celeste':
+            return 0
+          case 'violeta':
+            return 1
+          case 'naranja':
+            return 2
+          case 'verde':
+            return 3
+        }
+      }
+
+      iluminarSecuencia() {
+        for (let i = 0; i < this.nivel; i++) {
+          let color = this.transformarNumeroAColor(this.secuencia[i]);
+          setTimeout(() => {
+            this.iluminarColor(color)
+            switch (this.secuencia[i]) {
+              case 0:
+                audioDo.play()
+                break;
+              case 1:
+                audioRe.play()
+                break;
+              case 2:
+                audioMi.play()
+                break;
+              case 3:
+                audioFa.play()
+                break;
+            }
+          } , 1000 * i);
+        }
+      }
+
+      iluminarColor(color) {
+        this.colores[color].classList.add('light');
+        setTimeout(() => this.apagarColor(color), 350)
+      }
+
+      apagarColor(color) {
+        this.colores[color].classList.remove('light')
+      }
+
+      agregarEventoClick() {
+        this.colores.celeste.addEventListener('click', this.elegirColor)
+        this.colores.verde.addEventListener('click', this.elegirColor)
+        this.colores.violeta.addEventListener('click', this.elegirColor)
+        this.colores.naranja.addEventListener('click', this.elegirColor)
+      }
+
+      eliminarEventosClick() {
+        this.colores.celeste.removeEventListener('click', this.elegirColor)
+        this.colores.verde.removeEventListener('click', this.elegirColor)
+        this.colores.violeta.removeEventListener('click', this.elegirColor)
+        this.colores.naranja.removeEventListener('click', this.elegirColor)
+      }
+
+      elegirColor(ev) {
+        const nombreColor = ev.target.dataset.color;
+        const numeroColor = this.transformaColorANumero(nombreColor);
+        this.iluminarColor(nombreColor);
+        switch (numeroColor) {
           case 0:
             audioDo.play()
             break;
@@ -114,96 +174,88 @@ class Juego {
             audioFa.play()
             break;
         }
-      } , 1000 * i);
-    }
-  }
 
-  iluminarColor(color) {
-    this.colores[color].classList.add('light');
-    setTimeout(() => this.apagarColor(color), 350)
-  }
-
-  apagarColor(color) {
-    this.colores[color].classList.remove('light')
-  }
-
-  agregarEventoClick() {
-    this.colores.celeste.addEventListener('click', this.elegirColor)
-    this.colores.verde.addEventListener('click', this.elegirColor)
-    this.colores.violeta.addEventListener('click', this.elegirColor)
-    this.colores.naranja.addEventListener('click', this.elegirColor)
-  }
-
-  eliminarEventosClick() {
-    this.colores.celeste.removeEventListener('click', this.elegirColor)
-    this.colores.verde.removeEventListener('click', this.elegirColor)
-    this.colores.violeta.removeEventListener('click', this.elegirColor)
-    this.colores.naranja.removeEventListener('click', this.elegirColor)
-  }
-
-  elegirColor(ev) {
-    const nombreColor = ev.target.dataset.color;
-    const numeroColor = this.transformaColorANumero(nombreColor);
-    this.iluminarColor(nombreColor);
-    switch (numeroColor) {
-      case 0:
-        audioDo.play()
-        break;
-      case 1:
-        audioRe.play()
-        break;
-      case 2:
-        audioMi.play()
-        break;
-      case 3:
-        audioFa.play()
-        break;
-    }
-
-    if (numeroColor === this.secuencia[this.subnivel]) {
-      this.subnivel++;
-      if (this.subnivel === this.nivel) {
-        this.nivel++;
-        nivelActual.innerHTML = this.nivel;
-        if (this.nivel > nivelR) {
-          nivelRecord.innerHTML = this.nivel;
-          nivelR = this.nivel;
-        }
-        this.eliminarEventosClick()
-        if (this.nivel === (ULTIMO_NIVEL + 1)) {
-          this.ganoElJuego();
+        if (numeroColor === this.secuencia[this.subnivel]) {
+          this.subnivel++;
+          if (this.subnivel === this.nivel) {
+            this.nivel++;
+            nivelActual.innerHTML = this.nivel;
+            if (this.nivel > nivelR) {
+              nivelRecord.innerHTML = this.nivel;
+              nivelR = this.nivel;
+            }
+            this.eliminarEventosClick()
+            if (this.nivel === (ULTIMO_NIVEL + 1)) {
+              this.ganoElJuego();
+            } else {
+              setTimeout(this.siguienteNivel, 1500);
+            }
+          }
         } else {
-          setTimeout(this.siguienteNivel, 1500);
+            this.perdioElJuego();
         }
       }
-    } else {
-        this.perdioElJuego();
+
+      ganoElJuego() {
+        swal('¡Ganaste!', 'Felicitaciones, ganaste el juego :)', 'premio.png')
+          .then(this.inicializar)
+      }
+
+      perdioElJuego() {
+        swal('¡Lo siento!', 'perdiste el juego :(', 'error')
+          .then(() => {
+            this.eliminarEventosClick();
+            this.inicializar();
+          })
+      }
     }
-  }
 
-  ganoElJuego() {
-    swal('¡Ganaste!', 'Felicitaciones, ganaste el juego :)', 'premio.png')
-      .then(this.inicializar)
-  }
+    function empezarJuego() {
+      window.juego = new Juego()
+    }
 
-  perdioElJuego() {
-    swal('¡Lo siento!', 'perdiste el juego :(', 'error')
-      .then(() => {
-        this.eliminarEventosClick();
-        this.inicializar();
-      })
-  }
-}
+    function infoJuego() {
+      swal({
+        title: "¿Como se juega?",
+        text: "El juego de forma aleatoria va iluminando los cuadrantes de colores, y a la vez que se ilumina cada cuadrante emite un sonido propio. Después de esperar, el usuario debe ir introduciendo la secuencia mostrada en el orden correcto, ayudándose de su memoria visual y sonora. Si lo consigue, éste responderá con una secuencia más larga, y así sucesivamente. Si falla, el usuario debe volver a empezar. Los distintos niveles de dificultad van aumentando la velocidad de la secuencia a repetir.",
+        icon: "info",
+        button: "Ok",
+      });
+    }
 
-function empezarJuego() {
-  window.juego = new Juego()
-}
+} else {
+  const texto = document.getElementById('descargarChrome')
+  texto.classList.remove('hide');
+  const circulo = document.getElementById('gameboard')
+  // Estrellas
+  const estrella1 = document.getElementById('estrella1')
+  const estrella2 = document.getElementById('estrella2')
+  const estrella3 = document.getElementById('estrella3')
+  const estrella4 = document.getElementById('estrella4')
+  const estrella5 = document.getElementById('estrella5')
+  const estrella6 = document.getElementById('estrella6')
+  const estrella7 = document.getElementById('estrella7')
+  const meteoro = document.getElementById('meteoro')
+  // Buton
+  const btnEmpezar = document.getElementById('btnEmpezar')
+  var containerPoints = document.getElementById('containerPoints')
 
-function infoJuego() {
+  circulo.classList.add('hide');
+  estrella1.classList.add('hide');
+  estrella2.classList.add('hide');
+  estrella3.classList.add('hide');
+  estrella4.classList.add('hide');
+  estrella5.classList.add('hide');
+  estrella6.classList.add('hide');
+  estrella7.classList.add('hide');
+  meteoro.classList.add('hide');
+  btnEmpezar.classList.add('hide');
+  containerPoints.classList.add('hide');
+
   swal({
-    title: "¿Como se juega?",
-    text: "El juego de forma aleatoria va iluminando los cuadrantes de colores, y a la vez que se ilumina cada cuadrante emite un sonido propio. Después de esperar, el usuario debe ir introduciendo la secuencia mostrada en el orden correcto, ayudándose de su memoria visual y sonora. Si lo consigue, éste responderá con una secuencia más larga, y así sucesivamente. Si falla, el usuario debe volver a empezar. Los distintos niveles de dificultad van aumentando la velocidad de la secuencia a repetir.",
-    icon: "info",
+    title: "Lo Sentimos",
+    text: "Se recomienda el uso del navegador Google Chrome.",
+    icon: "chrome.png",
     button: "Ok",
   });
 }
